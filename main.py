2cd034baab43aae82673d264d4dda66b234ae9ac
ghifarit53 <59758342+ghifarit53@users.noreply.github.com>
2022-11-19 18:49:44 +0700

    feat: add main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+
+Dijkstra Visualizer
+
+Quiz 2
+Design & Analysis of Algorithm (IUP)
+Lecturer: Misbakhul Munir Irfan Subakti, S.Kom., M.Sc.
+
+Group Member
+- Muhammad Ghifari Taqiuddin (5025211063)
+- Mikhael Aryasatya Nugraha (5025211189)
+- Riski Ilyas (5025211062)
+
+"""
+
+import pygame
+import sys
+import random
+import math
+
+from collections import deque
+
+WIDTH, HEIGHT = 640, 480
+
+
+def main():
+    pygame.init()
+    root = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+
+
+if __name__ == "__main__":
+    main()
